b09da7877c0f8916634525536965065a0393f721
SpaceAlpaca12570 <SpaceAlpaca12570@gmail.com>
2021-07-29 17:17:51 -0600

    v 1.3 getting used to it working

--- app/src/main/java/com/example/vcontroltest/Main.java
+++ app/src/main/java/com/example/vcontroltest/Main.java
@@ -3,6 +3,6 @@ package com.example.vcontroltest;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        newGuy.printMessage() ;
+        newGuy.printMessage();
     }
 }
