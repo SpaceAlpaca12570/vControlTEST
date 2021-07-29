b8378f0c349fc4b32d2e54956226f60a68292568
SpaceAlpaca12570 <SpaceAlpaca12570@gmail.com>
2021-07-29 17:00:15 -0600

    v 1.1 adds newGuy class, calls from main

--- app/src/main/java/com/example/vcontroltest/Main.java
+++ app/src/main/java/com/example/vcontroltest/Main.java
@@ -1,10 +1,8 @@
 package com.example.vcontroltest;
 
-import newGuy;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        printmessage();
+        newGuy.printMessage();
     }
 }
