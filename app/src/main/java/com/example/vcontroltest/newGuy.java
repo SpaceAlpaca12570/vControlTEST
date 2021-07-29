ad5602e8c6a5d8a85463e081c24323e5694b815b
SpaceAlpaca12570 <SpaceAlpaca.FTC12570@gmail.com>
2021-07-29 17:27:41 -0600

    v 1.4 other newguy

--- app/src/main/java/com/example/vcontroltest/newGuy.java
+++ app/src/main/java/com/example/vcontroltest/newGuy.java
@@ -3,5 +3,6 @@ package com.example.vcontroltest;
 public class newGuy {
     public static void printMessage(){
         System.out.println("I'm the new guy");
+        System.out.println("the other new guy");
     }
 }
